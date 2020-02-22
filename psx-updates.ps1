4d34e3c681d60dd33f71285249e4c30f6ae0e29b
GodAtum <30117578+GodAtum360@users.noreply.github.com>
2020-02-22 21:19:52 +0000

    Create psx-updates.ps1

---
+++ psx-updates.ps1
@@ -0,0 +1,22 @@
+$password = Get-Content ‘C:\psx\cred.txt’ | ConvertTo-SecureString
+
+#Login to for pc1
+$credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist "pc1\psx",$password
+
+#Copy Aerowinx.jar to pc1
+$Session = New-PSSession -ComputerName 192.168.198.1 -Credential $credentials
+Copy-Item -Path C:\PSX\updates\Aerowinx.jar -Destination C:\PSX\Aerowinx\Aerowinx.jar -ToSession $session
+
+#Login to for pc2
+$credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist "pc2\psx",$password
+
+#Copy Aerowinx.jar to pc2
+$Session = New-PSSession -ComputerName 192.168.198.2 -Credential $credentials
+Copy-Item -Path C:\PSX\updates\Aerowinx.jar -Destination C:\PSX\Aerowinx\Aerowinx.jar -ToSession $session
+
+#Login to for pc3
+$credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist "pc3\psx",$password
+
+#Copy Aerowinx.jar to pc3
+$Session = New-PSSession -ComputerName 192.168.198.3 -Credential $credentials
+Copy-Item -Path C:\PSX\updates\Aerowinx.jar -Destination C:\PSX\Aerowinx\Aerowinx.jar -ToSession $session
